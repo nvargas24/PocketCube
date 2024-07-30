ddcb8f9a5698f8f5ad015226edea5cf41d7c93bf
nvargas24 <68164200+nvargas24@users.noreply.github.com>
2024-07-29 21:48:42 -0300

    Borro comentarios no relevantes y ordeno heard

--- Versión_original/AX25_LORA_DIGI_Transmit_Test_F/AX25_LORA_DIGI_Transmit_Test_F.ino
+++ Versión_original/AX25_LORA_DIGI_Transmit_Test_F/AX25_LORA_DIGI_Transmit_Test_F.ino
@@ -1,37 +1,18 @@
+/********  Librerias  ******/
 #include <RadioLib.h>
-
-SX1278 radio = new Module(10, 2, 9, 3);
-
 #include "settings.h"
-// include the library
-
 #include <EEPROM.h>
 #include <virtuabotixRTC.h>
 #include <Vcc.h>
 
+/************  Variables globales  ***********/
 const float VccMin = 3.0;                 // Minimum expected Vcc level, in Volts.
 const float VccMax = 4.15;                // Maximum expected Vcc level, in Volts.
 const float VccCorrection = 4.00 / 4.12;  // Measured Vcc by multimeter divided by reported Vcc
-
 //long interval = 60000;
-
 int state;
-//
-
-// this function is called when a complete packet
-// is received by the module
-// IMPORTANT: this function MUST be 'void' type
-//            and MUST NOT have any arguments!
-//
-// Note if you use an Wemos D1 as MCU, ICACHE_RAM_ATTR is required
-// https://community.blynk.cc/t/error-isr-not-in-iram/37426/20
-//ICACHE_RAM_ATTR
-
-// flag to indicate that a packet was received
-volatile bool receivedFlag = false;
-
-// disable interrupt when it's not needed
-volatile bool enableInterrupt = true;
+volatile bool receivedFlag = false; // flag to indicate that a packet was received
+volatile bool enableInterrupt = true; // disable interrupt when it's not needed
 
 int battVolts;
 int t_cpu;
@@ -50,22 +31,11 @@ float lastSNR = 0.0;
 
 int flag1 = 0;
 
-
 uint8_t uplinked[64];
 uint8_t downlink[64];
 uint8_t downlinklen = 0;
 
 Vcc vcc(VccCorrection);
-
-// SX1278 has the following connections:
-// NSS pin:   10
-// DIO0 pin:  2
-// RESET pin: 9
-// DIO1 pin:  3
-
-//SX1278 radio = new Module(10, 2, 9, 3);
-
-virtuabotixRTC myRTC(8, 7, 6);
 
 char cadenaTemporal[7];
 char cadenaTemporal_1[7];
@@ -75,13 +45,17 @@ char cadenaTemporal_3[7];
 int periodo = 5000;
 unsigned long TiempoAhora = 0;
 
+/*********  Obejtos globales  *********/
+SX1278 radio = new Module(10, 2, 9, 3);  // Creo objeto para nuevo modulo Lora
+/* SX1278 has the following connections:
+   NSS pin:   10
+   DIO0 pin:  2
+   RESET pin: 9
+   DIO1 pin:  3
+*/
+//SX1278 radio = new Module(10, 2, 9, 3);
 
-// or using RadioShield
-// https://github.com/jgromes/RadioShield
-//SX1278 radio = RadioShield.ModuleA;
-
-// create AX.25 client instance using the FSK module
-//AX25Client ax25(&radio);
+virtuabotixRTC myRTC(8, 7, 6); // Creo objeto para manejo RTC
 
 
 void setup() {
@@ -98,15 +72,12 @@ void setup() {
     // Reset button or otherwise some software reset
     // Serial.println("Reset button was pressed.");
     _RSC = EEPROM.read(7);
-
     _RSC = _RSC + 1;
 
     EEPROM.write(7, _RSC);
 
     // Serial.print("RST = ");
-
     // Serial.println(_RSC);
-
 
     MCUSR = 0;
 
@@ -158,14 +129,10 @@ void loop() {
   char BattVoltage[10];
 
   dtostrf(vcc.Read_Volts(), 4, 2, BattVoltage);
-
   dtostrf(GetTemp(), 3, 1, InternalTemp);
-
   analogReference(INTERNAL);
+  delay(10);}
 
-
-
-  delay(10);
   for (int lap = 0; lap <= 2; lap = lap + 1) {
 
     CTx = (analogRead(A2) / 8) - 7;  //CORRIENTE QUE CONSUME EL TRANSMISOR SIN MPPT menos lo que consume el led del opto -7 mA
@@ -175,10 +142,7 @@ void loop() {
 
   sprintf(txbuffer, "LW2DTZ %02d%02d%02d %sV %sC %dmA %d", myRTC.hours, myRTC.minutes, myRTC.seconds, BattVoltage, InternalTemp, CTx, _RSC);
   sprintf(superbuffer, "%s\n", txbuffer);
-
   delay(1000);
-
-
 
   setuprx();
 
@@ -190,27 +154,21 @@ void loop() {
 
   while (millis() < TiempoAhora + periodo) {
     // DIGI();
-
     // Rxloop();
     looprx();
   }
 
   // sendPACKET();
-
   delay(1000);
-
   sendLoRa();
 
   delay(2000);
-
   AfskSetup();
 
   delay(1000);
-
   AfskSetup();
 
   delay(1000);
-
   memset(superbuffer, 0, sizeof(superbuffer));
 
   //radio.standby();
